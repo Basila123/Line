c5a3a16089fadc6352a21273b8f44a816e4181de
Basila123 <basilanoushad92@gmail.com>
2021-12-03 04:26:23 +0530

    Added UC2

--- src/com/company/Length.java
+++ src/com/company/Length.java
@@ -1,5 +1,7 @@
 package com.company;
-import java.util.*;
+import java.util.Scanner;
+
+
 public class Length {
 
     public int x1,x2,y1,y2;
@@ -10,22 +12,24 @@ public class Length {
         this.y2 = y2;
     }
 
+   
+
     public double calculateLengthOfTheLine(){
         return Math.sqrt(Math.pow((this.x1-this.x2),2) + Math.pow((this.y1-this.y2),2));
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the co-ordinate x1");
-            int x1 = sc.nextInt();
-            System.out.println("Enter the co-ordinate y1");
-            int y1 = sc.nextInt();
-            System.out.println("Enter the co-ordinate x2");
-            int x2 = sc.nextInt();
-            System.out.println("Enter the co-ordinate y2");
-            int y2 = sc.nextInt();
-            Length lengthOfLineObj = new Length(x1,x2,y1,y2);
-        System.out.println("The Length of the Line from the given co-ordinates : " + lengthOfLineObj.calculateLengthOfTheLine());
+        System.out.println("Enter the co-ordinate x1");
+        int x1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y1");
+        int y1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x2");
+        int x2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y2");
+        int y2 = sc.nextInt();
+        Length length= new Length(x1,x2,y1,y2);
+        System.out.println("The Length of the Line from the given co-ordinates : " + length.calculateLengthOfTheLine());
 
     }
 }
